8d0925a7d84dd6e604a9eecbbc218a438a263963
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-10-25 12:40:48 +0600

    courses added

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const courses = require("./course.json");
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("my server running");
 });
 
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("course id: ", id);
+
+  const course = courses.find((cr) => cr.id === id || {});
+  res.send(course);
+});
 app.listen(port, () => {
   console.log(`my server is running on: ${port}`);
 });
